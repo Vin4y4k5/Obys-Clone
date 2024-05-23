3bf1129df11d360b1545d2e494a1528813022ee0
vinayak4523 <random105264@gmail.com>
2024-05-23 22:39:32 +0530

    Image Animation Done

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ tl.to(".line h2" , {
 tl.to("#loader", {
   opacity:0,
   duration:0.3,
-  delay:2, // --  change this to 2.
+  delay:0, // --  change this to 2.
 })
 
 tl.from("#page1" , {
@@ -103,8 +103,18 @@ function cursorAnimation(){
   Shery.makeMagnet("#nav-part2 h3", {});
 }
 
+function imageAnimation(){
+  Shery.imageEffect(".image-div" , {
+    style:5,
+    config:{"a":{"value":2,"range":[0,30]},"b":{"value":-0.94,"range":[-1,1]},"zindex":{"value":"9996999","range":[-9999999,9999999]},"aspect":{"value":0.7999927559853671},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":false},"maskVal":{"value":1,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.46,"range":[0,10]},"metaball":{"value":0.31,"range":[0,2]},"discard_threshold":{"value":0.47,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.49,"range":[0,2]},"noise_scale":{"value":11.45,"range":[0,100]}},
+    gooey:true,
+  })
+}
+
+
 locomotive()
 loadingAnimation()
 cursorAnimation()
+imageAnimation()
 
 // 1:25:50 git add .
